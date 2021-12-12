9f1593361793ffa3b5d249d07c40e08c19868c21
DerianCardenas <47439867+DerianCardenas@users.noreply.github.com>
2021-12-12 12:08:17 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+function comic() {
+    fetch("http://gateway.marvel.com/v1/public/comics?ts=1000&apikey=2a63f5bcddf74a9597df7407e5cc6a4b&hash=674805c8f6220783eb43e645e46b5ede", {
+        "method": "GET"
+    })
+    .then(response => response.json())
+    .then(data => console.log(data.data));
+}
+comic();
+const header = ()=>{
+    const  menuBtn = document.querySelector('.btn_menu'),
+    menu = document.querySelector('.menu_items');
+    menuBtn.addEventListener('click',()=>{
+        if(menu.classList.contains("active")){
+            alert("Cerrando menú");
+            menu.classList.remove("active");
+        }
+        else{
+            alert("Abriendo menu");
+            menu.classList.add("active");
+        }
+    });
+}
+
+document.addEventListener('DOMContentLoaded',header);
+
---
+++ js/scrollcharac.js
@@ -0,0 +1,49 @@
+let down = document.querySelector('.down-p');
+let limit =100, offset =100, cont=1;
+down.onclick=()=>{
+    fetch("http://gateway.marvel.com/v1/public/characters?ts=1000&apikey=2a63f5bcddf74a9597df7407e5cc6a4b&hash=674805c8f6220783eb43e645e46b5ede&offset="+offset*cont+"&limit="+limit, {
+            "method": "GET"
+        })
+        .then(response => response.json())
+        .then(data => loop(data.data));
+    cont++;
+}
+function loop(data){
+    console.log(data);
+    for(var i = 0; i < data.results.length; i++){
+        console.log(data.results[i]);
+        showChar(data.results[i]);
+    }
+}
+function showChar(char){
+    let cont = document.getElementById("cont");
+    let flipcont = document.createElement('div');
+    let card = document.createElement('div');
+    let frente = document.createElement('div');
+    let dorso = document.createElement('div');
+    let imgCont = document.createElement('div');
+    let img = document.createElement('img');
+    let text = document.createElement('p');
+    let textinfo = document.createElement('p');
+    flipcont.className="flip-container";
+    card.className="card";
+    frente.className="frente";
+    dorso.className="dorso";
+    imgCont.className="imgCont";
+    let source = char.thumbnail.path+"."+char.thumbnail.extension;
+    img.src=source;
+    text.innerText=char.id+" "+char.name;
+    textinfo.innerText="Hero Identifier: "+char.id+" "
+    if(char.description=="")
+        textinfo.innerText="Information Not Found.."
+    else
+        textinfo.innerText=char.description;
+    imgCont.appendChild(img);
+    frente.appendChild(imgCont);
+    frente.appendChild(text);
+    dorso.appendChild(textinfo);
+    card.appendChild(dorso);
+    card.appendChild(frente);
+    flipcont.appendChild(card);
+    cont.appendChild(flipcont);
+}
